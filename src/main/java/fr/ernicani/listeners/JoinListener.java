ed52e03cccd99735021ba67b43d7882bce29defa
Thomas Lindeker <45493414+ernivani@users.noreply.github.com>
2023-03-05 23:14:04 +0100

    teamManager and change block colored and change the color of the block depends on the team of the user and if the player have no team he can't send anything and todo: get the player team of the player hitted and if it's the same team cancel the event or disable friendly fire

--- src/main/java/fr/ernicani/listeners/JoinListener.java
+++ src/main/java/fr/ernicani/listeners/JoinListener.java
@@ -4,6 +4,7 @@ import fr.ernicani.Splatgames;
 import fr.ernicani.manager.GameManager;
 import fr.ernicani.manager.GameState;
 import fr.ernicani.manager.PlayerManager;
+import fr.ernicani.manager.TeamState;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
@@ -16,7 +17,6 @@ import java.util.Objects;
 public class JoinListener implements Listener {
     private GameManager gameManager;
 
-    private GameState gameState;
 
     public JoinListener(GameManager gameManager) {
         this.gameManager = gameManager;
@@ -31,13 +31,17 @@ public class JoinListener implements Listener {
         Bukkit.broadcastMessage("état de la partie : "+gameManager.getGameState().toString() + " " + GameState.LOBBY.toString());
 
         player.sendMessage(gameManager.getGameState() != GameState.LOBBY ? "§cLa partie est en cours" : "§aLa partie n'est pas en cours");
-           if (gameManager.getGameState() == GameState.LOBBY) {
-                player.setGameMode(GameMode.SURVIVAL);
-                player.teleport(gameManager.getLobbyLocation());
-            } else {
-                player.setGameMode(GameMode.SPECTATOR);
-                player.teleport(gameManager.getLobbyLocation());
-            }
+        if (gameManager.getGameState() == GameState.LOBBY) {
+            player.setGameMode(GameMode.SURVIVAL);
+            player.teleport(gameManager.getLobbyLocation());
+        } else {
+            player.setGameMode(GameMode.SPECTATOR);
+            player.teleport(gameManager.getLobbyLocation());
+        }
+
+
+        gameManager.setTeamState(TeamState.BLUE,player);
+        player.sendMessage(gameManager.getTeamState(player).toString());
 
     }
 
--- src/main/java/fr/ernicani/listeners/onPlayerInteract.java
+++ src/main/java/fr/ernicani/listeners/onPlayerInteract.java
@@ -1,14 +1,13 @@
 package fr.ernicani.listeners;
 
 import fr.ernicani.manager.GameManager;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Arrow;
+import fr.ernicani.manager.GameState;
+import fr.ernicani.manager.TeamState;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class onPlayerInteract implements Listener {
@@ -26,6 +25,15 @@ public class onPlayerInteract implements Listener {
         }
         Player player = event.getPlayer();
         ItemStack item = event.getItem();
+
+        if (gameManager.getTeamState(player) == TeamState.NONE) {
+            return;
+        }
+
+        if (gameManager.getGameState() != GameState.ACTIVE) {
+            return;
+        }
+
         if (item.getType().name().contains("STICK")) {
 
             Snowball snowball = player.launchProjectile(Snowball.class);
