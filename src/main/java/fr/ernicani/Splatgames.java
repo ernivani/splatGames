81bb8dc3dae9254b3255180cabd6b42afefb1c83
Thomas <tlindeker0@gmail.com>
2023-03-06 15:23:27 +0100

    Update Splatgames.java

--- src/main/java/fr/ernicani/Splatgames.java
+++ src/main/java/fr/ernicani/Splatgames.java
@@ -5,7 +5,6 @@ import fr.ernicani.commands.MenuCommand;
 import fr.ernicani.commands.StartCommand;
 import fr.ernicani.listeners.*;
 import fr.ernicani.manager.GameManager;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Splatgames extends JavaPlugin {
@@ -14,25 +13,27 @@ public final class Splatgames extends JavaPlugin {
     private static Splatgames plugin;
 
 
+
+
+
     @Override
     public void onEnable() {
         super.onEnable();
 
         this.gameManager = new GameManager(this);
 
-        getServer().getPluginManager().registerEvents(new BlockBreakListener(gameManager), this);
-        getServer().getPluginManager().registerEvents(new JoinListener(gameManager), this);
-        getServer().getPluginManager().registerEvents(new LeaveListener(gameManager), this);
-        getServer().getPluginManager().registerEvents(new onPlayerInteract(gameManager), this);
-        getServer().getPluginManager().registerEvents(new ProjectileHit(gameManager), this);
+
+        ListenersHandle.registerListeners(this, gameManager);
+
+
+
+
+
 
 
         getCommand("start").setExecutor(new StartCommand(gameManager));
         getCommand("setlobby").setExecutor(new LobbySet(gameManager));
-        //getCommand("menu").setExecutor(new MenuCommand(gameManager));
-
-
-        //todo: load the config file
+        getCommand("menu").setExecutor(new MenuCommand(gameManager));
 
 
         getLogger().info("Splatgames is enabled");
